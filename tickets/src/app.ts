b9d73a4d0380072cdf4f2ff02f434c8e2cc7bea2
Ivan <iyanez717@gmail.com>
2021-10-17 02:24:58 -0500

    mongo and ticket image and services, skaffold

--- tickets/src/app.ts
+++ tickets/src/app.ts
@@ -4,12 +4,6 @@ import { json } from 'body-parser';
 import { errorHandler, NotFoundError } from '@iytickets/common';
 import cookieSession from 'cookie-session';
 
-import { CurrentUserRouter } from './routes/current-user';
-import { signinRouter } from './routes/singnin';
-import { signoutRouter } from './routes/singnout';
-import { signupRouter } from './routes/singnup';
-
-
 const app = express();
 app.set('trust proxy', true);
 app.use(json());
@@ -21,10 +15,6 @@ app.use(
   })
 );
 
-app.use(CurrentUserRouter);
-app.use(signinRouter);
-app.use(signoutRouter);
-app.use(signupRouter);
 app.all('*', async(req, res)=>{
   throw new NotFoundError();
 });
