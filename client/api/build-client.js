8e019644fcc847a069c61b26e7bbe4b25d54f3ca
Ivan <iyanez@subetuweb.site>
2021-11-01 02:47:53 -0600

    cookies segure

--- client/api/build-client.js
+++ client/api/build-client.js
@@ -4,7 +4,7 @@ const build_client = ({ req }) => {
   if(typeof window === 'undefined'){
     // we are on server!
     return axios.create({
-      baseURL: 'http://ingress-nginx-controller.ingress-nginx.svc.cluster.local', 
+      baseURL: 'http://www.bookgo.site/', //'http://ingress-nginx-controller.ingress-nginx.svc.cluster.local',
       headers: req.headers      
     });
   }else{
